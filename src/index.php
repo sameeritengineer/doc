297087060f77b8f336352a33d6f29d4799180452
sameer <sameer.ece564@gmail.com>
2024-07-28 17:50:28 +0530

    add files

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "db";
+$username = "user";
+$password = "password";
+$dbname = "mydatabase";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
---
+++ src/sameer.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Hello sameer how are you buddy</h1>";
+
+?>
